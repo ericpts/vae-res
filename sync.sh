7a152e494a02d6c56d52375c5c64002d4122ddd3
Eric Stavarache <ericptst@gmail.com>
2019-03-03 15:44:17 +0100

    Fix syncing

--- sync.sh
+++ sync.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-export nport=16911
-
 while true; do
     rsync -rav -e "ssh -p$nport" root@0.tcp.ngrok.io:~/checkpoints/ checkpoints
     rsync -rav -e "ssh -p$nport" root@0.tcp.ngrok.io:~/image_at_epoch_\*.png images/
