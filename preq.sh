c0033167f5fc398e5bc45847b670d9c93155d388
Eric Stavarache <ericptst@gmail.com>
2019-03-12 15:16:06 +0100

    Do not install cuda in preq.sh

--- preq.sh
+++ preq.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt install tmux vim zsh
-apt-get install cuda
+# apt-get install cuda
 pip3 install tf-nightly-gpu-2.0-preview
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
