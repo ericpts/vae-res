aa0730acf7c7c5bd97eadb92617327bd4f18392d
Eric Stavarache <ericptst@gmail.com>
2019-03-22 14:15:45 +0100

    Shuffle images when rendering test set

--- train.py
+++ train.py
@@ -44,7 +44,10 @@ def train_model(
     def test_step():
         test_loss = model.evaluate_on_dataset(D_test)
 
-        for (X, y) in D_test.take(config.num_examples).batch(config.num_examples):
+        for (X, y) in D_test.shuffle(
+                2**10).take(
+                    config.num_examples).batch(
+                        config.num_examples):
             (softmax_confidences, vae_images) = model.run_on_input(X)
             X_output = tf.reduce_sum(softmax_confidences * vae_images, axis=0)
             fname = 'images/{}/image_at_epoch_{}.png'.format(model.name, epoch)
