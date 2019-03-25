9028441bbc14a3bc0a917bcd89efd355b85f08d1
Eric Stavarache <ericptst@gmail.com>
2019-03-25 15:24:43 +0100

    Try to fix tf-nightly-gpu version

--- preq.sh
+++ preq.sh
@@ -2,6 +2,6 @@
 
 apt install tmux vim zsh
 # apt-get install cuda
-pip3 install tf-nightly-gpu-2.0-preview
+pip3 install tf-nightly-gpu-2.0-preview==2.0.0.dev20190325
 
 # sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
