862077742a795a28a0468dcd48d27c4c9b39b5b7
Eric Stavarache <ericptst@gmail.com>
2019-03-30 09:30:35 +0100

    Add run.sh script, for remote running on leonhard

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+shopt -s globstar
+
+dt=$(date '+%d-%m-%Y=%H-%M-%S')
+
+read -p "Enter experiment name: " expname
+read -p "Enter experiment description: " desc
+
+remote_dir="vae-res-${expname}-${dt}"
+
+rsync -rav -R **/*py preq.sh ericst@login.leonhard.ethz.ch:~/${remote_dir}
+
+ssh ericst@login.leonhard.ethz.ch <<EOF
+cd ${remote_dir}
+echo "${expname}" > experiment_name.txt
+echo "${desc}$" > experiment_desc.txt
+bsub -W 8:00 -n 4 -R "rusage[mem=4000,ngpus_excl_p=1]" "python3 train.py --name leonhard"
+EOF
+
