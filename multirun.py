1b14df3603b07357caff40a33496ec1c19656e22
Eric Stavarache <ericptst@gmail.com>
2019-04-10 14:26:39 +0200

    Add utility to run multiple experiments at once

---
+++ multirun.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+import yaml
+from pathlib import Path
+import subprocess
+
+to_run = [
+    {
+        'beta': 2,
+        'gamma': 0.01,
+        'nvaes': 3,
+    },
+    {
+        'beta': 1,
+        'gamma': 0.01,
+        'nvaes': 3,
+    },
+    {
+        'beta': 2,
+        'gamma': 0.01,
+        'nvaes': 4,
+    },
+    {
+        'beta': 1,
+        'gamma': 0.01,
+        'nvaes': 4,
+    },
+
+
+
+    {
+        'beta': 2,
+        'gamma': 0.005,
+        'nvaes': 3,
+    },
+    {
+        'beta': 1,
+        'gamma': 0.005,
+        'nvaes': 3,
+    },
+    {
+        'beta': 2,
+        'gamma': 0.005,
+        'nvaes': 4,
+    },
+    {
+        'beta': 1,
+        'gamma': 0.005,
+        'nvaes': 4,
+    },
+]
+
+def run_once(cfg: dict):
+    p = Path('cfg.yaml')
+    p.write_text(
+        yaml.dump(cfg, default_flow_style=False)
+    )
+
+    name = '_'.join([
+        f'{key}={value}' for (key, value) in cfg.items()
+    ])
+
+    proc = subprocess.run(
+        ['./run.sh', '--runs', '20',
+         '--name', name,
+         '--desc', 'test',
+        ],
+        check=True
+    )
+
+
+for cfg in to_run:
+    run_once(cfg)
