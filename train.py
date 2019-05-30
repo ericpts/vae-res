eaa213ba97e62bcab90e075c22081856b93c284d
Eric Stavarache <ericptst@gmail.com>
2019-05-30 10:45:23 +0200

    Save pictures more rarely

--- train.py
+++ train.py
@@ -101,7 +101,7 @@ def train_model(
         with test_summary_writer.as_default():
             test_loss, test_imgs = test_step()
 
-            if epoch % 1 == 0:
+            if epoch % 5 == 0:
                 save_test_pictures(test_imgs, epoch)
 
         if epoch % 40 == 0:
