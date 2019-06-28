1cb1b77b13b3f494cd78e8c47e724542d33d0eb5
Eric Stavarache <ericptst@gmail.com>
2019-06-28 11:54:43 +0200

    Work on thesis

--- thesis/auto/thesis.el
+++ thesis/auto/thesis.el
@@ -5,12 +5,12 @@
                      '(("memoir" "11pt" "a4paper" "titlepage")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("fontenc" "OT1") ("babel" "english") ("inputenc" "utf8") ("mathpazo" "sc") ("ntheorem" "amsmath" "thmmarks") ("hyperref" "linkcolor=black" "colorlinks=true" "citecolor=black" "filecolor=black")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
